5b67e75b4d00a6eb6893e1e493a0a9202c75ec4f
Unknown <p.tomlinson@mdx.ac.uk>
2017-10-29 12:52:40 +0000

    Movement Exqample

---
+++ Movement/Assets/PT_scripts/PT_Basic_Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PT_Basic_Move : MonoBehaviour {
+    public float fl_MovementSpeed = 1;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //When you press M the item moves up
+        if(Input.GetButtonUp("M"))
+        {
+            transform.Translate(0, fl_MovementSpeed, 0);
+        }
+
+    }
+}
+
+
+//Input.GetAxis("Vertical")
+//Input.GetAxis("Horizontal")
