b3a9e587ec98439dbd16f7bdaebe0a6ce6c9341a
Penda <p.tomlinson@mdx.ac.uk>
2020-01-27 09:04:06 +0000

    Physics Play Elements Added

---
+++ ProjectBase/Assets/Scripts/MouseSwitch.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSwitch : MonoBehaviour {
+
+    private BoxCollider Target;
+    public float timeLeft = 30.0f;
+    public Material changeMaterial;
+
+    private bool countingDown = false;
+    private Material startMaterial;
+
+    private Renderer _Renderer;
+
+    // Use this for initialization
+    void Start () {
+
+        _Renderer = gameObject.GetComponent<Renderer>();
+        startMaterial = _Renderer.material;
+         
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (countingDown)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                Target.isTrigger = true;
+                _Renderer.material = startMaterial;
+                ResetTimer();
+            }
+        }
+
+    }
+
+    public void SwitchOver()
+    {
+        Target = GetComponent<BoxCollider>();
+        Target.isTrigger = false;
+        _Renderer.material = changeMaterial;
+        countingDown = true;
+    }
+
+    private void ResetTimer()
+    {
+        timeLeft = 30.0f;
+        countingDown = false;
+    }
+
+}
---
+++ ProjectBase/Assets/Scripts/MouseTurnOn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTurnOn : MonoBehaviour {
+    private MouseSwitch mouseSwitchScript;
+    private Ray ray;
+    private RaycastHit hit;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                mouseSwitchScript = hit.collider.gameObject.GetComponent<MouseSwitch>();
+                if (mouseSwitchScript != null)
+                {
+                    mouseSwitchScript.SwitchOver();
+                }
+            }
+        }
+    }
+}
