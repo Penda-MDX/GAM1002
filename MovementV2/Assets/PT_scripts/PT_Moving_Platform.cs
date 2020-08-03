79a1325f027f5abe78a3a251690288ad9f457012
Penda <p.tomlinson@mdx.ac.uk>
2020-08-03 09:54:37 +0100

    Change to  fixedupdate on moving platform
    'fixes' problems with character controller

--- MovementV2/Assets/PT_scripts/PT_Moving_Platform.cs
+++ MovementV2/Assets/PT_scripts/PT_Moving_Platform.cs
@@ -25,7 +25,7 @@ public class PT_Moving_Platform : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (!paused||!pauseAtEnd)
         {
